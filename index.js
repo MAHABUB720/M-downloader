b846b7a68f682905d7f2424fb1ce0b8aee17f41b
༆...𝗠𝗛𝗔𝗕𝗨𝗕✞︎𝗥𝗔𝗛𝗠𝗔𝗡..༄ <mahabub678919@gmail.com>
2024-09-06 06:05:23 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,78 +1,16 @@
-const cheerio = require('cheerio'),
-  request = require('request'),
-  ndown = require('nayan-media-downloader'),
-  { alldown } = require('nayan-media-downloader')
+const { alldl } = require('rahad-all-downloader');
 
-function tikdown(url, callback) {
-  const query = 'q=' + encodeURIComponent(url) + '&lang=en',
-    options = {
-      url: 'https://savetik.co/api/ajaxSearch',
-      method: 'POST',
-      headers: headers, // Assuming headers is defined elsewhere
-      body: query,
-    }
-  
-  request(options, function (error, response, body) {
-    if (error) {
-      return callback(error, null)
-    }
-    if (response.statusCode !== 200) {
-      return callback(
-        new Error('Unexpected status code: ' + response.statusCode),
-        null
-      )
-    }
+async function dl(url) {
     try {
-      const result = JSON.parse(body)
-      if (typeof result.data !== 'string') {
-        return callback(new Error('Data is not in expected format'), null)
-      }
-      const $ = cheerio.load(result.data),
-        downloadLink = $('.tik-right .dl-action a').attr('href'),
-        responseData = {
-          author: 'Mahabub Rahman',
-          contact: 'https://www.facebook.com/www.xnxx.com.140',
-          data: downloadLink,
-        }
-      callback(null, responseData)
+        const response = await alldl(url);
+        return response;
     } catch (error) {
-      callback(error, null)
+        console.error('Error in downloading:', error.message);
+        return { error: 'Download failed' };
     }
-  })
 }
 
-async function fndown(url) {
-  try {
-    const result = await ndown(url)
-    return {
-      author: 'Mahabub Rahman',
-      contact: 'https://www.facebook.com/www.xnxx.com.140',
-      data: result.data,
-    }
-  } catch (error) {
-    throw new Error('Failed to fetch URL: ' + error.message)
-  }
-}
-
-async function alldl(url) {
-  if (!url) {
-    throw new Error('URL is required')
-  }
-  try {
-    const result = await alldown(url)
-    return {
-      Author: 'Mahabub Rahman',
-      devfb: 'https://www.facebook.com/www.xnxx.com.140',
-      devwp: 'wa.me/+8801312737981',
-      data: result.data,
-    }
-  } catch (error) {
-    throw new Error(error.message)
-  }
-}
 
 module.exports = {
-  tikdown: tikdl,
-  fndown: fndl,
-  alldl: aldl,
+    dl
 }
